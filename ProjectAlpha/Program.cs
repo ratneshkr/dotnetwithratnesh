194a9500471d218cdda25e23611e67596b8db76a
Ratnesh Dheeraj <rdheeraj.in@gmail.com>
2020-10-16 08:55:22 +0545

    commit 3

--- ProjectAlpha/Program.cs
+++ ProjectAlpha/Program.cs
@@ -6,17 +6,17 @@ namespace ProjectAlpha
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("\nWhat is your name? ");
-            //var name = Console.ReadLine();
-            //var date = DateTime.Now;
+            Console.WriteLine("\nWhat is your name? ");
+
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
 
             //Output
-            //Console.WriteLine($"\nHello, {name}, on {date:d} at {date:t}!");
+            Console.WriteLine($"\n Hello, {name}, on {date:d} at {date:t}!");
 
 
-            int x = 10 + 2 * 2;
-            Console.WriteLine(x);
-                
+            //int x = 10 + 2 * 2;
+            //Console.WriteLine(x);
 
             Console.Write("\nPress any key to exit...");
             Console.ReadKey(true);
